7e00a21a69fb27536c9ebe1186130bdb2db3720c
Jeff Thierch <jefferson08_alexandre@hotmail.com>
2021-09-14 19:08:51 -0300

    Requisitos 02 - 05 - Completo

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const inputText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text')
+const imageMeme = document.getElementById('meme-image')
+const imageInput = document.getElementById('meme-insert')
+let imagemUploaded = ''
 
 function insertTextMeme() {
   memeText.innerHTML = inputText.value
 }
 
+function addImageToContainer (event) {
+	imageMeme.src = URL.createObjectURL(event.target.files[0]);
+}
+
 inputText.addEventListener('keyup', insertTextMeme)
+imageInput.addEventListener('change', addImageToContainer)
