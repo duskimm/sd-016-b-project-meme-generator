6e4085f982750cbcd5985a154bb1e66c615cd97f
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-14 17:49:40 -0300

    requisito 6 concluido

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ function readImage() {
 }
 
 document.getElementById("meme-insert").addEventListener("change", readImage);
+
+let container = document.getElementById('meme-image-container');
+
+let borderClasses = ['fire-border', 'water-border', 'earth-border'];
+
+let buttons = document.getElementsByTagName('button');
+
+function addBorder(event) {
+  let className = event.target.innerText.toLowerCase();
+
+  if (container.className !== className) {
+    container.className = className;
+  } else {
+    container.className = '';
+  }
+
+}
+
+for (let index = 0; index < buttons.length; index += 1) {
+  buttons[index].addEventListener('click', addBorder);
+}
+
+// let waterBtn = document.getElementById('water');
+
+// function addBorder() {
+//   let color = window.getComputedStyle(fireBtn).getPropertyValue('background-color');
+//   createBorder('5px', 'dashed', color);
+// }
+
+// waterBtn.addEventListener('click', addBorder)
