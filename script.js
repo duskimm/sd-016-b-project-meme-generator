9f9913e8a2fa3db588ada679c4debedb5790b196
Juliane Cardoso <julianesilvac75@gmail.com>
2021-09-14 18:25:08 -0300

    corrige erros lint

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-let textInput = document.getElementById('text-input');
-
 function insertText(event) {
   document.getElementById('meme-text').innerText = event.target.value;
 }
 
-textInput.addEventListener('keyup', insertText);
-
-let imageInput = document.getElementById('meme-insert');
+document.getElementById('text-input').addEventListener('keyup', insertText);
 
 // Função que adiciona imagem
 
 function readImage() {
   if (this.files && this.files[0]) {
-    let file = new FileReader();
+    const file = new FileReader();
     file.readAsDataURL(this.files[0]);
-    file.onload = function(e) {
-      document.getElementById("meme-image").src = e.target.result;
+    file.onload = function (e) {
+      document.getElementById('meme-image').src = e.target.result;
     };
   }
   // Créditos: Matheus Battisti em horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/
 }
 
-document.getElementById("meme-insert").addEventListener("change", readImage);
+document.getElementById('meme-insert').addEventListener('change', readImage);
 
-let container = document.getElementById('meme-image-container');
+const container = document.getElementById('meme-image-container');
 
-let borderClasses = ['fire-border', 'water-border', 'earth-border'];
-
-let buttons = document.getElementsByTagName('button');
+const buttons = document.getElementsByTagName('button');
 
 function addBorder(event) {
-  let className = event.target.innerText.toLowerCase();
+  const className = event.target.innerText.toLowerCase();
 
   if (container.className !== className) {
     container.className = className;
@@ -43,10 +37,10 @@ for (let index = 0; index < buttons.length; index += 1) {
   buttons[index].addEventListener('click', addBorder);
 }
 
-let readyMemes = document.getElementsByClassName('ready-images');
+const readyMemes = document.getElementsByClassName('ready-images');
 
 function addReadyMeme(event) {
-  let img = event.target.src;
+  const img = event.target.src;
 
   document.getElementById('meme-image').src = img;
 }
