f688bf50a124234c2c49f151895e2e2661a467ac
Matheus Nugas <matheusnugas@gmail.com>
2021-09-13 23:30:44 -0300

    Bordas extras adicionadas

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
+const memeContainer = document.querySelector('#meme-image-container');
+const fireButton = document.querySelector('#fire');
+const waterButton = document.querySelector('#water');
+const earthButton = document.querySelector('#earth');
 
 window.addEventListener('load', function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
@@ -13,11 +17,38 @@ window.addEventListener('load', function() {
         }
     });
   });
+  // Funcao encontrada em https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html 
 
-// Funcao encontrada em https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html 
+
 
 function addText () {
     memeText.innerHTML = textInput.value
 }
-
 textInput.addEventListener('keyup', addText);
+
+function changeFire () {
+    if (memeContainer.style.border === '3px dashed red') {
+        memeContainer.style.border = '1px solid black'
+    } else {
+        memeContainer.style.border = '3px dashed red'
+    }
+}
+fireButton.addEventListener('click', changeFire)
+
+function changeWater () {
+    if (memeContainer.style.border === '5px double blue') {
+        memeContainer.style.border = '1px solid black'
+    } else {
+        memeContainer.style.border = '5px double blue'
+    }
+}
+waterButton.addEventListener('click', changeWater)
+
+function changeEarth () {
+    if (memeContainer.style.border === '6px groove green') {
+        memeContainer.style.border = '1px solid black';
+    } else {
+        memeContainer.style.border = '6px groove green';
+    }
+}
+earthButton.addEventListener('click', changeEarth)
