2bb7c15210a1a4413456f2213b8589a4f3187535
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-14 22:22:01 -0300

    falta carregar imagem e adicionar bordas

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
+function mostraTexto(e) {
+  document.querySelector('#meme-text').innerText = e.target.value;
+}
+
+function atualizaFoto(e) {
+  if (e.target.classList.contains('memes')) {
+    document.querySelector('#meme-image-container').style.backgroundImage = `url(${e.target.src})`;
+  }
+}
+
+function configurar() {
+  document.querySelector('#text-input').addEventListener('input', mostraTexto);
+  document.querySelector('#meme-1').addEventListener('click', atualizaFoto);
+}
+
+window.onload = configurar;
+
+// REQUISITOS FALTANTES
+// carregar imagem
+// botões de bordas
 
 // Linha em branco é de graça né linter
