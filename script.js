2a1e59bb07cfca13d0b0f5b0b8f56e3485b436cd
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-18 17:49:31 -0300

    C3 Requisito Bonus 6

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ function memeTxt() {
   memeTx.innerText = memT;
 }
 
-const memeText = document.querySelector('#text-input');
-memeText.addEventListener('input', memeTxt);
-
 function memeImg() {
   const reader = new FileReader();
   reader.addEventListener('load', () => {
@@ -20,12 +17,32 @@ function memeImg() {
   });
   reader.readAsDataURL(this.files[0]);
 }
+function border(event) {
+  const meDiv = document.querySelector('.meme_container');
+  const eTar = event.target;
+  const eBorCol = window.getComputedStyle(eTar);
+  const corBor = eBorCol.getPropertyValue('background-color');
+  if (this.id === 'fire') {
+    meDiv.style.border = `3px dashed ${corBor}`;
+  } else if (this.id === 'water') {
+    meDiv.style.border = `5px double ${corBor}`;
+  } else if (this.id === 'earth') {
+    meDiv.style.border = `6px groove ${corBor}`;
+  }
+}
 
+const test = document.querySelector('#meme-insert');
+test.addEventListener('change', memeImg);
+const memeText = document.querySelector('#text-input');
+memeText.addEventListener('input', memeTxt);
+
+const redBtn = document.querySelector('#fire');
+redBtn.addEventListener('click', border);
+const greenBtn = document.querySelector('#water');
+greenBtn.addEventListener('click', border);
+const blueBtn = document.querySelector('#earth');
+blueBtn.addEventListener('click', border);
 // function tstImg(event) {
 //   const image = document.getElementById('output');
 //   image.src = URL.createObjectURL(event.target.files[0]);
 // }
-
-// const test = document.querySelector('#meme-insert');
-const test = document.querySelector('#meme-insert');
-test.addEventListener('change', memeImg);
