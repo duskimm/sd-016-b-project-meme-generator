afb254c54de04390588ac41b2a93be4168163af8
leandro <leandrochagask@gmail.com>
2021-09-19 14:43:59 -0300

    Requisito 02

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 
 let textInput = document.querySelector('#text-input');
 let memeText = document.querySelector('#meme-text');
+let memeInsert = document.querySelector('#meme-insert');
+let memeImage = document.querySelector('#meme-image');
 
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value;
   console.log(textInput.value);
 })
 
-// <input id="text-input" type="text">
-// 		<div id="meme-image-container">
-// 			<div id="meme-text"></div>
+const fileReader = new FileReader();
+
+function src() {
+  memeImage.src = fileReader.result;
+}
+
+memeInsert.addEventListener('change', () => {
+  let file = memeInsert.files[0];
+  fileReader.onload = src;
+  fileReader.readAsDataURL(file);
+});
