ed7c52c4ee88a60432375cc86ff42ef9eec8a31b
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-20 00:41:15 -0300

    Imagem e texto - ok

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const textInput2 = document.getElementById('text-input');
 
+// Função para limpar o texto anterior.
 function clearText() {
   const textArea = document.getElementById('meme-text');
   textArea.innerText = '';
 }
 
+// Função para inserir o novo texto digitado.
 function memeText() {
   const textInput = document.getElementById('text-input').value;
   const texto = document.getElementById('meme-text');
   texto.innerText = textInput;
 }
-
-function createButton() {
-  const button = document.createElement('button');
-  button.innerText = 'Aplicar Texto';
-  document.body.appendChild(button);
-}
-createButton();
-
 textInput2.addEventListener('change', memeText);
 
+// Função para alterar a imagem.
 function searchImg(event) {
   const img = document.getElementById('meme-image');
   img.src = URL.createObjectURL(event.target.files[0]);
@@ -27,3 +22,11 @@ function searchImg(event) {
 const imgInsert = document.getElementById('meme-insert');
 
 imgInsert.addEventListener('change', searchImg);
+
+
+// function createButton() {
+//   const button = document.createElement('button');
+//   button.innerText = 'Aplicar Texto';
+//   document.body.appendChild(button);
+// }
+// createButton();
