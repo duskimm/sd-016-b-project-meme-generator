529d52b2aa2d16952263f6a1f604b662a4786873
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2021-09-21 00:35:50 -0300

    Todos os requisitos - OK

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const textInput2 = document.getElementById('text-input');
-
-// Função para limpar o texto anterior.
-function clearText() {
-  const textArea = document.getElementById('meme-text');
-  textArea.innerText = '';
-}
+const container = document.getElementById('meme-image-container');
+const buttonContainer = document.getElementById('button-container');
 
 // Função para inserir o novo texto digitado.
 function memeText() {
@@ -12,7 +8,7 @@ function memeText() {
   const texto = document.getElementById('meme-text');
   texto.innerText = textInput;
 }
-textInput2.addEventListener('change', memeText);
+textInput2.addEventListener('keyup', memeText);
 
 // Função para alterar a imagem.
 function searchImg(event) {
@@ -20,13 +16,60 @@ function searchImg(event) {
   img.src = URL.createObjectURL(event.target.files[0]);
 }
 const imgInsert = document.getElementById('meme-insert');
-
+// Evento para alterar a imagem.
 imgInsert.addEventListener('change', searchImg);
 
+// Função para criar botões.
+function createButton(name, id) {
+  const button = document.createElement('button');
+  button.innerText = name;
+  button.id = id;
+  buttonContainer.appendChild(button);
+}
+createButton('Fire', 'fire');
+const fireButton = document.getElementById('fire');
+fireButton.style.backgroundColor = 'red';
 
-// function createButton() {
-//   const button = document.createElement('button');
-//   button.innerText = 'Aplicar Texto';
-//   document.body.appendChild(button);
-// }
-// createButton();
+// Evento para o botão Fire.
+fireButton.addEventListener('click', () => {
+//  const container = document.getElementById('meme-image-container');
+  if (container.className !== 'fire') {
+    container.className = 'fire';
+  } else {
+    container.className = 'defaultBorder';
+  }
+});
+
+// Funcão para criar e adicionar evento ao botão water.
+createButton('Water', 'water');
+const waterButton = document.getElementById('water');
+waterButton.style.backgroundColor = 'blue';
+waterButton.addEventListener('click', () => {
+  if (container.className !== 'water') {
+    container.className = 'water';
+  } else {
+    container.className = 'defaultBorder';
+  }
+});
+
+// Função para criar e adicionar evento ao botão earth.
+createButton('Earth', 'earth');
+const earthButton = document.getElementById('earth');
+earthButton.style.backgroundColor = 'green';
+earthButton.addEventListener('click', () => {
+  if (container.className !== 'earth') {
+    container.className = 'earth';
+  } else {
+    container.className = 'defaultBorder';
+  }
+});
+
+const containerImages = document.getElementById('imagens-memes-reference');
+
+containerImages.addEventListener('click', (event) => {
+  const alvo = event.target;
+  const img = document.getElementById('meme-image');
+  if (alvo.src !== undefined) {
+    img.src = alvo.src;
+  }
+});
