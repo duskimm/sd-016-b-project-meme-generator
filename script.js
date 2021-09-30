de0f4e418185aa127180074b8f0e71c18866ba9c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2021-09-30 15:26:30 -0300

    agora funciona carregar a imagem local

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ function atualizaFoto(e) {
   }
 }
 
+function novaImagem() {
+  // https://pt.stackoverflow.com/questions/383341/utiliza%C3%A7%C3%A3o-do-input-file-com-js
+  const reader = new FileReader();
+  reader.readAsDataURL(document.querySelector('#meme-insert').files[0]);
+  reader.onload = () => {
+    document.querySelector('#meme-image-container').style.backgroundImage = `
+    url(${reader.result})`;
+  };
+}
+
 function configurar() {
   document.querySelector('#text-input').addEventListener('input', mostraTexto);
   document.querySelector('#meme-1').addEventListener('click', atualizaFoto);
+  document.querySelector('#meme-insert').addEventListener('change', novaImagem);
 }
 
 window.onload = configurar;
