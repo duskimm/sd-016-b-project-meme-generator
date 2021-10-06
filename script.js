1234f973c55e086fbd6e40292e35ad7e6c43559e
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-10-06 10:55:25 -0300

    Arrumando lint

--- script.js
+++ script.js
@@ -7,28 +7,35 @@ const fire = document.querySelector('#fire');
 const water = document.querySelector('#water');
 const earth = document.querySelector('#earth');
 
+// colocar borda inicial no container
 memecontainer.style.border = '1px solid black';
 
 getInput.addEventListener('keyup', () => {
   getText.innerHTML = getInput.value;
 });
 
-getImg.addEventListener('change', (event) => {
+// função para pegar a url do input e colocar no preview
+getImg.addEventListener('change', (event) => { // retirado a ideia de usar o URL.createObjectUrl do site https://stackoverflow.com/questions/11708797/inserting-a-file-input-as-an-img-in-the-dom
   const urlImg = URL.createObjectURL(event.target.files[0]);
   memeImagem.src = `${urlImg}`;
 });
 
+// botão fire
 fire.addEventListener('click', () => {
   memecontainer.style.border = '3px dashed red';
 });
+
+// botão water
 water.addEventListener('click', () => {
   memecontainer.style.border = '5px double blue';
 });
 
+// botão earth
 earth.addEventListener('click', () => {
   memecontainer.style.border = '6px groove green';
 });
 
+// atualizar o preview com os memes prontos
 const getPai = document.querySelector('#divPai');
 getPai.addEventListener('click', (event) => {
   memeImagem.src = `${event.target.src}`;
