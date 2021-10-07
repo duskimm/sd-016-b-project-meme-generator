dc2d32d7ed0f6902eede797bd7b7de615be49ea7
Serafim <guilhermeserafimbonfim@hotmail.com>
2021-10-07 12:14:26 -0300

    resolve todos os requisitos

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
 const photoText = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
-const fileChoose = document.getElementById('meme-insert');
-let output = document.getElementById('output');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
+const imgContainer = document.getElementById('meme-image-container');
+const firstImg = document.getElementById('meme-1');
+const secondImg = document.getElementById('meme-2');
+const thirdImg = document.getElementById('meme-3');
+const fourthImg = document.getElementById('meme-4');
+const output = document.getElementById('meme-image');
 
 function printText () {
-  if (photoText.value != '') {
+  if (photoText.value !== '') {
     memeText.innerText = photoText.value;
   }
 }
 photoText.addEventListener('keyup', printText);
 
-
-let loadFile = function(event) {  
+// função loadFile retirada desse link, não entendi 100% dela, mas funcionou https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+const loadFile = function (event) {
   output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function() {
-    URL.revokeObjectURL(output.src) // free memory
-  }
+  output.onload = function () {
+    URL.revokeObjectURL(output.src);
+  };
 };
-// fileChoose.addEventListener('click', loadFile)
-// window.onload = function start () {
-// }
+
+fireButton.addEventListener('click', () => {
+  imgContainer.className = 'fire';
+});
+
+waterButton.addEventListener('click', () => {
+  imgContainer.className = 'water';
+});
+
+earthButton.addEventListener('click', () => {
+  imgContainer.className = 'earth';
+});
+
+firstImg.addEventListener('click', (event) => {
+  const targets = event.target;
+  if (targets.src !== undefined) {
+    output.src = targets.src;
+  }
+});
+
+secondImg.addEventListener('click', (event) => {
+  const targets = event.target;
+  if (targets.src !== undefined) {
+    output.src = targets.src;
+  }
+});
+
+thirdImg.addEventListener('click', (event) => {
+  const targets = event.target;
+  if (targets.src !== undefined) {
+    output.src = targets.src;
+  }
+});
+
+fourthImg.addEventListener('click', (event) => {
+  const targets = event.target;
+  if (targets.src !== undefined) {
+    output.src = targets.src;
+  }
+});
