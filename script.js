ba2f904661e031a4312bccf608d750f505b7508b
Millene Galetti <mikagaletti1@gmail.com>
2021-10-06 21:06:39 -0300

    Requisito 5 e alguns itens do Requisito 3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const memeText = document.querySelector('#meme-text');
 const textInput = document.querySelector('#text-input');
 const memeContainer = document.querySelector('#meme-image-container');
+const imgInput = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
 
 function inputText() {
   const text = textInput.value;
@@ -8,3 +10,13 @@ function inputText() {
 }
 
 textInput.addEventListener('keyup', inputText);
+
+function addImage(img) {
+  const imgUrl = URL.createObjectURL(img.target.files[0]);
+  console.log(imgUrl);
+  memeImage.src = imgUrl;
+  memeContainer.style.width = '500px';
+  memeContainer.style.height = '500px';
+}
+
+imgInput.addEventListener('change', addImage);
