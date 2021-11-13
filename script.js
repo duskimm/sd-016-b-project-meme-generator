a1e635750a72667b7adb7521065e00c0861d12e9
Eduardo Fradique <edsfradique@gmail.com>
2021-11-13 01:20:17 -0300

    :memo: requisitos 7

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const input = document.getElementById('text-input');
+const inputIMG = document.getElementById('meme-insert');
+const inputTXT = document.getElementById('text-input');
 const span = document.getElementById('meme-text');
+const output = document.getElementById('meme-image');
 const container = document.getElementById('meme-image-container');
 const fire = document.getElementById('fire');
 const water = document.getElementById('water');
 const earth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
-const loadFile = (event) => {
-  const output = document.getElementById('meme-image');
-  output.src = URL.createObjectURL(event.target.files[0]);
+inputIMG.onchange = () => {
+  const [file] = inputIMG.files;
+  if (file) {
+    output.src = URL.createObjectURL(file);
+  }
 };
 
-input.addEventListener('input', (event) => {
+inputTXT.addEventListener('input', (event) => {
   span.textContent = event.target.value;
 });
 
@@ -24,4 +32,20 @@ water.addEventListener('click', () => {
 
 earth.addEventListener('click', () => {
   container.style.border = '6px groove green';
+});
+
+meme1.addEventListener('click', (event) => {
+  output.src = event.target.src;
+});
+
+meme2.addEventListener('click', (event) => {
+  output.src = event.target.src;
+});
+
+meme3.addEventListener('click', (event) => {
+  output.src = event.target.src;
+});
+
+meme4.addEventListener('click', (event) => {
+  output.src = event.target.src;
 });
