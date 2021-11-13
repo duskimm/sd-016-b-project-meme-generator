c5dbd734f567491895ffb0078ccc79540b763307
Eduardo Fradique <edsfradique@gmail.com>
2021-11-12 23:11:42 -0300

    :memo: requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,13 @@
+const input = document.getElementById('text-input');
+const span = document.getElementById('meme-text');
+
+const loadFile = (event) => {
+  const output = document.getElementById('meme-image');
+  output.src = URL.createObjectURL(event.target.files[0]);
+};
+
+input.addEventListener('input', (event) => {
+  span.textContent = event.target.value;
+});
+
+window.onload = () => {};
