08c5cad6219045611546d0b3247c8c33060db306
Eduardo Fradique <edsfradique@gmail.com>
2021-11-13 00:06:50 -0300

    :memo: requisitos 6

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const input = document.getElementById('text-input');
 const span = document.getElementById('meme-text');
+const container = document.getElementById('meme-image-container');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
 
 const loadFile = (event) => {
   const output = document.getElementById('meme-image');
@@ -8,4 +12,16 @@ const loadFile = (event) => {
 
 input.addEventListener('input', (event) => {
   span.textContent = event.target.value;
+});
+
+fire.addEventListener('click', () => {
+  container.style.border = '3px dashed red';
+});
+
+water.addEventListener('click', () => {
+  container.style.border = '5px double blue';
+});
+
+earth.addEventListener('click', () => {
+  container.style.border = '6px groove green';
 });
